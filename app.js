f783cad8b8d9a3ee9d0d1b0069a398c9d9c32002
Jiri Burant <jiriburant@microsoft.com>
2022-08-30 10:24:12 +0200

    Fixing imports.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { CallClient, CallAgent } from "@azure/communication-calling";
-import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+import { CallClient } from "./node_modules/@azure/communication-calling";
+import { AzureCommunicationTokenCredential } from './node_modules/@azure/communication-common';
 
 let call;
 let callClient;
